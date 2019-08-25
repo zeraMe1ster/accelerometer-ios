ecba8bbd76bbfe7d97a4e25b45044bdf579e75ca
zeraMe1ster <sauhard.eg@gmail.com>
2019-08-25 15:54:12 +0545

    Works ok for part 1

--- xyz-accelerometer/xyz-accelerometer/MainScreenVC.swift
+++ xyz-accelerometer/xyz-accelerometer/MainScreenVC.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import CoreMotion
+
 
 class MainScreenVC: UIViewController {
 
+    @IBOutlet weak var xLbl: UILabel!
+    @IBOutlet weak var yLbl: UILabel!
+    @IBOutlet weak var zLbl: UILabel!
+    
+    var motionManager: CMMotionManager!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateValues)
+        
+        
     }
 
+    func updateValues(data: CMAccelerometerData?, error: Error?) {
+        guard let acceleroData = data else {return}
+        
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+        
+        let x = formatter.string(for: acceleroData.acceleration.x)!
+        let y = formatter.string(for: acceleroData.acceleration.y)!
+        let z = formatter.string(for: acceleroData.acceleration.z)!
+        
+        xLbl.text = "X: \(x)"
+        yLbl.text = "Y: \(y)"
+        zLbl.text = "Z: \(z)"
+        
+        
+        
+    }
 
 }
 
